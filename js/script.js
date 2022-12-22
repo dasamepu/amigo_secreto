fe320a996446630a565eeda6a382aea2d6a5abd2
david <dasamepu@gmail.com>
2022-12-22 16:02:42 -0500

    AI

--- js/script.js
+++ js/script.js
@@ -1,25 +1,29 @@
-const const_resultado = document.getElementById("id_resultado")
+// secret-friend.js
 
-family = ["Angel",
-        "Santi",
-        "Ofe",
-        "Octavio",
-        "Nacho",
-        "Lora",
-        "Helenita",
-        "Has",
-        "Zito",
-        "Marranita",
-        "Yiyer",
-        "Tatis",
-        "Vale", 
-        "Antoni",
-        "Jorge",
-        "Adriana",
-        "Irene",
-        "Vivis",
-        "Victor"]
+// Initialize an array to store the names
+let names = [];
 
-let persona_resultante = family[2]
+// Get the form element
+const form = document.querySelector('form');
 
-const_resultado.innerHTML = persona_resultante
+// Add a submit event listener to the form
+form.addEventListener('submit', (event) => {
+  // Prevent the default form submission behavior
+  event.preventDefault();
+
+  // Get the name from the input field
+  const name = document.querySelector('#name').value;
+
+  // Add the name to the array
+  names.push(name);
+
+  // Clear the input field
+  document.querySelector('#name').value = '';
+});
+
+// Select and match two random names from the array
+const name1 = names[Math.floor(Math.random() * names.length)];
+const name2 = names[Math.floor(Math.random() * names.length)];
+
+// Print the match to the console
+console.log("Match: " + name1 + " and " + name2);
