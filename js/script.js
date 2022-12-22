54a6c1e0b8962c6b8cd7dfb4dfa2015f54b7a8c6
david <dasamepu@gmail.com>
2022-12-22 13:56:43 -0500

    js

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const const_resultado = document.getElementById("id_resultado")
+
+family = ["Angel",
+        "Santi",
+        "Ofe",
+        "Octavio",
+        "Nacho",
+        "Lora",
+        "Helenita",
+        "Has",
+        "Zito",
+        "Marranita",
+        "Yiyer",
+        "Tatis",
+        "Vale", 
+        "Antoni",
+        "Jorge",
+        "Adriana",
+        "Irene",
+        "Vivis",
+        "Victor"]
+
+let persona_resultante = family[2]
+
+const_resultado.innerHTML = persona_resultante
